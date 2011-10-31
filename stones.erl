21a6ccecb2ef94af4714e8ad40a95ef24631e0b2
Alex Shneyderman <a.shneyderman@gmail.com>
2011-11-01 00:45:54 +0100

    a bit of a clean up

--- stones.erl
+++ stones.erl
@@ -8,8 +8,6 @@ main(_Args) ->
                                           St4 <- lists:seq(1,37), 
                                           (St1 + St2 + St3 + St4) == 40 ],
 
-    io:format("~p possible combinations generated.~n", [length(StoneWeights)]),
-
     % Find all the weight combinations that could be our solutions
     Solutions = [ Weights || Weights <- StoneWeights, solves_for_all_X(Weights)],
 
@@ -20,7 +18,6 @@ main(_Args) ->
 % exists for each of the X ranging from 1 to 40 and Sign1..Sign4 
 % could take on values -1, 0 and 1. 
 solves_for_all_X( { St1, St2, St3, St4 } ) ->
-    %io:format("~p~n", [{ St1, St2, St3, St4 }]),
     lists:all( fun(X) ->
                  length( [{Sgn1,Sgn2,Sgn3,Sgn4} || Sgn1 <- [1, 0, -1],
                                                    Sgn2 <- [1, 0, -1],
@@ -28,22 +25,3 @@ solves_for_all_X( { St1, St2, St3, St4 } ) ->
                                                    Sgn4 <- [1, 0, -1],
                                                    (X + Sgn1 * St1 + Sgn2 * St2 + Sgn3 * St3 + Sgn4 * St4) == 0 ] ) /= 0
                end, lists:seq(1,40) ).
-
-%% stone_configs() -> [ {St1,St2,St3,St4} || St1 <- lists:seq(1,37), 
-%%                                           St2 <- lists:seq(1,37), 
-%%                                           St3 <- lists:seq(1,37), 
-%%                                           St4 <- lists:seq(1,37), 
-%%                                           (St1 + St2 + St3 + St4) =:= 40 ].
-%% %
-%% % We need to make sure that at least one of the possible equations for 
-%% % X (+/-) S1 (+/-) S2 (+/-) S3 (+/-) S4 = 0
-%% % exists for each of the X ranging from 1 to 40
-%% %
-%% sloves( {St1,St2,St3,St4} ) ->
-%%     [ {St1,St2,St3,St4} || X    <- lists:seq(1,40), 
-%%                            Sgn1 <- [1, -1],
-%%                            Sgn2 <- [1, -1],
-%%                            Sgn3 <- [1, -1],
-%%                            Sgn4 <- [1, -1],
-%%                            X + Sgn1 * St1 + Sgn2 * St2 + Sgn3 * St3 + Sgn4 * St4 =:= 0 ].
-
